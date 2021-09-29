9e37a49bfb4759f8634cc6a637ff829211da82e6
Planck <maxplanck817@gmail.com>
2021-09-29 18:19:46 +0530

    major,update

--- Laplace.py
+++ Laplace.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from numpy.lib.function_base import average
 import time as t
 
-
 def gauss1d(x):
     y = np.array(x)
     for j in range(10000):
@@ -12,11 +11,13 @@ def gauss1d(x):
             y[i]=(y[i-1]+y[i+1])/2
     return(y)
 
-def sor2dpoisson(x,overcf=1.9,charge=[3,2,2]):
+def sor2dpoisson(x,overcf=1.9,charge=[[23,10,10],[35,23,-10]]):
     k,m = x.shape[0],x.shape[1]
     h = np.zeros(x.shape)
-    if h is not None :
-        h[charge[0],charge[1]] = charge[2]
+    if charge is not None :
+        for jk in charge:
+            h[jk[0]][jk[1]] = jk[2]
+
     for f in range(1000):
         new_x= np.array(x)    #IMP : duplicates array and avoids linking 
         for i in range(1,k-1):
@@ -28,31 +29,32 @@ def sor2dpoisson(x,overcf=1.9,charge=[3,2,2]):
             break
         else:
             x = new_x
-            
+
     return(new_x)
 
 if __name__ == "__main__":
     gd = 50
-    #starting potentials 
+    #starting potentials
     INPUT=np.ones((gd,),dtype="single")*5
     INPUT2D = np.ones((gd,gd),dtype="single") 
-    ##Dirichlet Boundary values
+    #Dirichlet Boundary values
     boundary_var = [1,0]
-    boundary_var2d = np.ones(gd,)*0
-    INPUT2D[0],INPUT2D[-1] = boundary_var2d ,boundary_var2d /2
+    boundary_var2d = np.ones(gd,)*10
+    INPUT2D[0],INPUT2D[-1] = boundary_var2d ,-1*boundary_var2d
+    INPUT2D.T[0],INPUT2D.T[-1] =  boundary_var2d/2 ,-1*boundary_var2d/2
     INPUT[0],INPUT[-1] = boundary_var[0],boundary_var[-1]
     
-    #fig = plt.figure()
-    #ax = plt.axes(projection="3d")
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")
     #x=np.linspace(5,100,100)
     #plt.plot(x,gauss1d(INPUT))
     x = np.linspace(-5,5,gd)
     y = np.linspace(-5,5,gd)
     X, Y = np.meshgrid(x, y)
     #print(func2d(INPUT2D))
-    #ax.plot_surface(X,Y,sor2dpoisson(INPUT2D), rstride=1, cstride=1,cmap='winter', edgecolor='none')
     t0 = t.time()
-    plt.pcolormesh(sor2dpoisson(INPUT2D))
+    #plt.pcolormesh(sor2dpoisson(INPUT2D))
+    ax.plot_surface(X,Y,sor2dpoisson(INPUT2D,charge=None), rstride=1, cstride=1, edgecolor='none')
     t1= t.time()
     print(t1-t0)
 
